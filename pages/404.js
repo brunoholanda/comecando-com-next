07a40a69f4fbd7fdf82453fb3eb6364307c5d80c
brunoholanda <holanda_rodrigues@hotmail.com>
2023-04-30 10:04:55 -0300

    estrutura de paginas e conceitos de API no next

---
+++ pages/404.js
@@ -0,0 +1,12 @@
+import Link from '../src/components/Link';
+export default function Page404 () {
+    return (
+        <div>
+            <h1>Voce se perdeu e caiu na pagina 404</h1>
+            <Link href="/">
+            ir para a HOME
+            </Link>
+            
+        </div>
+    )
+}
