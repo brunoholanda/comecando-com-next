2715abff1982211e292fc97aefefc417bff90c4a
brunoholanda <holanda_rodrigues@hotmail.com>
2023-04-30 11:17:52 -0300

    finalizando o projeto

--- pages/faq.js
+++ pages/faq.js
@@ -1,5 +1,29 @@
-import { useEffect, useState } from 'react';
+import FAQScreen from '../src/screens/FAQScreen';
+
+export default FAQScreen; 
+
+export async function getStaticProps() {
+    console.log('Em modo DEV, sempre roda! A cada acesso')
+    console.log('Roda SOMENTE em build time')
+    const FAQ_API_URL = 'https://gist.githubusercontent.com/omariosouto/0ceab54bdd8182cbd1a4549d32945c1a/raw/578ad1e8e5296fa048e3e7ff6b317f7497b31ad9/alura-cases-faq.json';
+    const faq = await fetch(FAQ_API_URL)
+        .then((respostaDoServidor) => {
+            return respostaDoServidor.json();
+        })
+        .then((resposta) => {
+            return resposta;
+        });
+
+    return {
+      props: {
+          qualquercoisa: 'que eu passar aqui',
+          faq,
+      },
+    };
+}  
+/*
 import Link from 'next/link';
+import PageTitle from '../src/components/PageTitle';
 
 export async function getStaticProps() {
 
@@ -28,6 +52,7 @@ export default function FAQPage ({faq}) {
 
     return (
         <div>
+             <PageTitle>FAQ - Alura Cases Campanha</PageTitle>
             <h1>Alura Cases - Pagina de perguntas</h1>
             <Link href="/">
             ir para a HOME
@@ -45,3 +70,4 @@ export default function FAQPage ({faq}) {
         </div>
     );
 }
+*/
